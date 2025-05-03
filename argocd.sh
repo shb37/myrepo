bc00d2429a7905b9d87a6d60265c2b34e68ee06d
er.mdshoib@hotmail.com <ershoib37@gmail.com>
2025-05-03 16:20:55 +0530

    argocd

---
+++ argocd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#argocd instalation script
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.4.7/manifests/install.yaml
+sleep 10
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl get svc argocd-server -n argocd -o json
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
