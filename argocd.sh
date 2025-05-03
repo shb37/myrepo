3f7a1caaadf7fb4d1fb2f34194eccd401fd79b0a
er.mdshoib@hotmail.com <ershoib37@gmail.com>
2025-05-03 16:35:32 +0530

    argocd

--- argocd.sh
+++ argocd.sh
@@ -7,4 +7,5 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2
 sleep 10
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 kubectl get svc argocd-server -n argocd -o json
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+sleep 10
+echo 'Argocd Password is ' $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) > pass.txt
