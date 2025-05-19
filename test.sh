32b56bc6cf8cf8ca3817baa980797ead557a18e4
er.mdshoib@hotmail.com <ershoib37@gmail.com>
2025-05-20 01:40:56 +0530

    updated

--- test.sh
+++ test.sh
@@ -2,13 +2,16 @@
 
 # -p is used to as flag and it avoids error also creat dir if already exists
 sudo mkdir -p mydir
+cd mydir
+sudo touch f1 f2 f3 f4
 
-sudo touch mydir/file
-
-# sudo tee is used to give root permission to write into file
+# sudo tee is used to give root permission to write into f1 and f2 in single command
 # -a: Append instead of overwrite.
 # -i: Ignore interrupts.
-echo "Hello world" | sudo tee -a mydir/file
-sudo grep world file
+echo "This is a random my world" | sudo tee -a f1 f2
+
+#or also can use this command to give sudo permission
+sudo sh -c 'echo "second method to write data" | tee f3 f4'
+
 
 
